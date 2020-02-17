dfc04c6f3fefe2e8ae4be074ce8e5d42f3d476f6
LucasSimone <46714774+LucasSimone@users.noreply.github.com>
2020-02-17 17:25:01 -0500

    expanded main for different user childs

--- main.cpp
+++ main.cpp
@@ -1,22 +1,40 @@
 // Main class to run the std in loop that will take the user inputs
 #include <iostream>
 #include <string>
-#include "./Users/User.h"
 #include "./Authentication/Authentication.h"
+#include "./Users/User.h"
+#include "/Users/BuyStandard/BuyStandard.h"
+#include "/Users/SellStandard/SellStandard.h"
+#include "/Users/FullStandard/FullStandard.h"
+#include "/Users/Admin/Admin.h"
+
 using namespace std;
+
+void state0();
+void state2Buy(string username);
+void state2Sell(string username);
+void state2Full(string username);
+void state2Admin(string username);
 
 int main()
 {
-    int transaction;
-    User currentUser;
-    int state = 0;
-    //States:
-    //State 0 = Logged out
-    //State 1 = Exit state
-    //State 2 = Logged in state
+    state0();
+    return 0;
+}
 
-    //Logged out state
-    while (state == 0)
+//States:
+//State 0 = Logged out
+//State 1 = Exit state
+//State 2 = Logged in state
+
+void state0()
+{
+    int transaction;
+    string username;
+    int accntType;
+    bool loop = true;
+
+    while (loop)
     {
         //Ouput to user
         cout << "Welcome to the auction website. Please select an option from the following:" << endl;
@@ -30,42 +48,161 @@ int main()
         //Make choice based on user input
         switch (transaction)
         {
-            //Exit is chosen 
-            case 0: state = 1;
-                    break;
+        //Exit is chosen 
+        case 0: loop = false;
+            break;
 
-            //Login was chosen
-            case 1: Authentication auth;//Create instance of authentication class
-                    currentUser = auth.login();//Call login function
-                    if (currentUser.getUsername() != "") { state = 2; }//Check if valid login
-                    break;
+         //Login was chosen
+        case 1: 
+            Authentication auth;//Create instance of authentication class
+            cout << "Enter Username: ";
+            cin >> username;
+            accntType = auth.login(username);//Call login function
+
+            switch (accntType)
+            {
+            case 0:
+                break;
+            case 1:
+                state2Buy(username);
+                break;
+            case 2:
+                state2Sell(username);
+                break;
+            case 3:
+                state2Full(username);
+                break;
+            case 4:
+                state2Admin(username);
+                break;
+            default:
+                break;
+            }
+
+            break;
 
             //Default case 
-            default: break;
+            default: 
+                break;
         }
-        //Logged in state
-        while (state == 2)
-        {
-            //Output to user
-            cout << "Please choose from one of the following options:" << endl;
-            cout << "Logout -- 1 " << endl;
-            cout << "-->";
-
-            //Take user input
-            cin >> transaction;
-
-            //Make choice based on user input
-            switch (transaction)
-            {
-            case 1:  state = 0;
-                     break;
-            }
-        }
-        
-
     }
-
-
-    return 0;
-
 }
+
+
+void state2Buy(string username)
+{
+    BuyStandard user(username, 0);
+    int transaction;
+    bool loop = true;
+    while (loop)
+    {
+        //Output to user
+        cout << "Account Type: Buy Standard" << endl;
+        cout << "Please choose from one of the following options:" << endl;
+        cout << "Logout -- 1 " << endl;
+        cout << "Refund -- 2 " << endl;
+        cout << "Add credit -- 3 " << endl;
+        cout << "Bid -- 4 " << endl;
+        cout << "-->";
+
+        //Take user input
+        cin >> transaction;
+
+        //Make choice based on user input
+        switch (transaction)
+        {
+        case 1:  loop = false;
+            break;
+        }
+    }
+}
+
+void state2Sell(string username)
+{
+    SellStandard user(username, 0);
+    int transaction;
+    bool loop = true;
+    while (loop)
+    {
+        //Output to user
+        cout << "Account Type: Sell Standard" << endl;
+        cout << "Please choose from one of the following options:" << endl;
+        cout << "Logout -- 1 " << endl;
+        cout << "Refund -- 2 " << endl;
+        cout << "Add Credit -- 3 " << endl;
+        cout << "Advertise -- 4 " << endl;
+        cout << "-->";
+
+        //Take user input
+        cin >> transaction;
+
+        //Make choice based on user input
+        switch (transaction)
+        {
+        case 1:  loop = false;
+            break;
+        }
+    }
+}
+
+void state2Full(string username)
+{
+    FullStandard user(username, 0);
+    int transaction;
+    bool loop = true;
+    while (loop)
+    {
+        //Output to user
+        cout << "Account Type: Full Standard" << endl;
+        cout << "Please choose from one of the following options:" << endl;
+        cout << "Logout -- 1 " << endl;
+        cout << "Refund -- 2 " << endl;
+        cout << "Add Credit -- 3 " << endl;
+        cout << "Bid -- 4 " << endl;
+        cout << "Advertise -- 5 " << endl;
+        cout << "-->";
+
+        //Take user input
+        cin >> transaction;
+
+        //Make choice based on user input
+        switch (transaction)
+        {
+        case 1:  loop = false;
+            break;
+        }
+    }
+}
+
+void state2Admin(string username)
+{
+    Admin user(username, 0);
+    int transaction;
+    bool loop = true;
+    while (loop)
+    {
+        //Output to user
+        cout << "Account Type: Admin" << endl;
+        cout << "Please choose from one of the following options:" << endl;
+        cout << "Logout -- 1 " << endl;
+        cout << "Refund -- 2 " << endl;
+        cout << "Add Credit -- 3 " << endl;
+        cout << "Bid -- 4 " << endl;
+        cout << "Advertise -- 5 " << endl;
+        cout << "Create User -- 6 " << endl;
+        cout << "Delete User -- 7 " << endl;
+        cout << "Add Credit to user account -- 4 " << endl;
+        cout << "-->";
+
+        //Take user input
+        cin >> transaction;
+
+        //Make choice based on user input
+        switch (transaction)
+        {
+        case 1:  loop = false;
+            break;
+        }
+    }
+}
+
