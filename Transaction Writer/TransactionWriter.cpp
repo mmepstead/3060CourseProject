cf57149f51252672efb1dc8c489da198b1ecbbf3
Matthew Mepstead <MatthewMepstead@gmail.com>
2020-02-17 19:02:47 -0500

    Transaction Writer

---
+++ Transaction Writer/TransactionWriter.cpp
@@ -0,0 +1,22 @@
+#include "TransactionWriter.h"
+// Inputs: Transaction type(1-create, 2-delete etc.),  
+//  values: map containing the values for fields like username and others
+// Outputs: None
+void TransactionWriter::dailyTransactionWriter(int transactionType, map<string, string> values) 
+{
+
+}
+// Inputs: username: username of the user, type: the acronym for the user's account type(FS - full-standard),
+//  credit: the amount of credit on the user's account
+// Outputs: None
+void TransactionWriter::currentUsersWriter(string username, string type, float credit) 
+{
+
+}
+// Inputs: name: Name of item, seller: username of seller, winningUser: username of user with current highest bid,
+//  winningBid: value of current highest bid, daysLeft: Number of days left before auction closes
+// Outputs: None
+void TransactionWriter::availableItemsWriter(string name, string seller, string winningUser, string winningBid, int daysLeft) 
+{
+
+}
