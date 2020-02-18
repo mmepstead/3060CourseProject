9de78ed90e5c164e07cba129bba6c685ea86a4f7
LucasSimone <46714774+LucasSimone@users.noreply.github.com>
2020-02-17 19:24:14 -0500

    Added comments

--- Users/User.cpp
+++ Users/User.cpp
@@ -4,26 +4,32 @@
 #include "User.h"
 using namespace std;
 
+	// Inputs: String buyer: username of buyer, String seller: username of seller,
+	// float credit: amount of creddit to refund
+	// Outputs: None
 	void User::refund(string buyer, string seller, float credit)
 	{
 
 	}
 
 
-	/*
-	*/
+	// Inputs: float for amount of credit to add
+	// Outputs: float for new user balance
 	float User::addCredit(float credit)
 	{
 		if (credit <= 0 || credit > 999.99)
 		{
 			cout << "Invalid amont." << endl;
+			return 0;
 		}
 		else if ((creditBalance + credit) > 999.99)
 		{
 			cout << "Error: Adding amount " << credit << " will bring account balance above 999.99." << endl;
+			return 0;
 		}
 		else
 		{
 			cout << "Credit of " << credit << " successfully added to account" << getUsername() << endl;
+			return credit + creditBalance;
 		}
 }
