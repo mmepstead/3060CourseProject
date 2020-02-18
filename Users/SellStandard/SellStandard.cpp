32e968bb4a60ac58f44d571185c9d4b61f57cb6e
LucasSimone <46714774+LucasSimone@users.noreply.github.com>
2020-02-17 19:50:38 -0500

    Added Comments

--- Users/SellStandard/SellStandard.cpp
+++ Users/SellStandard/SellStandard.cpp
@@ -1,5 +1,8 @@
-#include "User.h"
+#include "../User.h"
 #include "SellStandard.h"
+
+// Inputs: string item: name of item, float amount: amount of bid
+// Outputs: None
 void SellStandard::bid(string item, float amount)
 {
 	cout << "Error: Account of type Sell Standard does not have acess to bid function" << endl;
--- Users/SellStandard/SellStandard.h
+++ Users/SellStandard/SellStandard.h
@@ -1,8 +1,10 @@
-#include "User.h"
+#include "../User.h"
 
 class SellStandard : public User
 {
 public:
+	// Inputs: string name: username for account, float balance: balance for account
+	// Outputs: None
 	SellStandard(string name, float balance)
 	{
 		setUsername(name);
@@ -10,5 +12,7 @@ public:
 		setUserType(2);
 	}
 
+	// Inputs: string item: name of item, float amount: amount of bid
+	// Outputs: None
 	void bid(string item, float amount);
 };
