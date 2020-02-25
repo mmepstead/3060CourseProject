7309dae3a574bd7b5a61f879a17606370f6d9a80
Matthew Mepstead <MatthewMepstead@gmail.com>
2020-02-25 13:26:32 -0500

    Testing script

--- test.sh
+++ test.sh
@@ -7,7 +7,9 @@ do
     results="$(dirname "$file")/results.txt"
     ../auction ../currentUsers.txt < $file > $output
     if diff --strip-trailing-cr $output $expected_output
-    then : echo "true" 
-    else : echo "false"
+    then
+        echo "Test Passed!" >> $results
+    else
+        echo "Test Failed!" >> $results
     fi
 done
