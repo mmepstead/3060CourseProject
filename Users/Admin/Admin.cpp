835a518ded0d2e3137b1d0ce1f07fbec1d151914
LucasSimone <46714774+LucasSimone@users.noreply.github.com>
2020-02-28 14:30:03 -0500

    Added create and delete functions

--- Users/Admin/Admin.cpp
+++ Users/Admin/Admin.cpp
@@ -1,5 +1,8 @@
 #include "../User.h"
 #include "Admin.h"
+#include "../../TransactionWriter/TransactionWriter.h"
+#include <map>
+
 // Inputs: string username: username to add credit to, float credit: amount to add to account
 // Outputs: float for new balance
 float Admin::addCredit(string username, float credit) {
@@ -24,4 +27,26 @@ float Admin::addCredit(string username, float credit) {
 			cout << "Current balance: " << creditBalance << endl;
 			return creditBalance;
 		}
+}
+
+void Admin::create(string username, int type)
+{
+  TransactionWriter writer;
+  map<string, string> values;
+  values.insert(pair<string, string>("username", username));
+  values.insert(pair<string, string>("user_type", to_string(type)));
+  values.insert(pair<string, string>("credit_balance", "000000000"));
+  writer.dailyTransactionWriter(1, values);
+  cout << "Successfully created user: " << username << endl;
+}
+
+void Admin::deleteUser(string username)
+{
+  TransactionWriter writer;
+  map<string, string> values;
+  values.insert(pair<string, string>("username", username));
+  values.insert(pair<string, string>("user_type", to_string(0)));
+  values.insert(pair<string, string>("credit_balance", "000000000"));
+  writer.dailyTransactionWriter(1, values);
+  cout << "Successfully deleted user: " << username << endl;
 }
